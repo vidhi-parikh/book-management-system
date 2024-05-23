8bc67ae737e49278c86a20c7ca8d382fa5ecfcfc
vidhi-parikh <parikhvidhi10@gmail.com>
2024-05-23 23:57:25 +0530

    add dummy data for testing

---
+++ frontend/src/data/bookStore.js
@@ -0,0 +1,7 @@
+export const bookStore = [
+    { _id: 1,title: "Book One", author: "Author One", year: 2001, genre: "Fiction" },
+    { title: "Book Two", author: "Author Two", year: 2002, genre: "Non-Fiction" },
+    { title: "Book Three", author: "Author Three", year: 2003, genre: "Science" },
+    { title: "Book Four", author: "Author Four", year: 2004, genre: "History" },
+  ];
+  
