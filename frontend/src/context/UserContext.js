096e9e344f69d9d646ee61f52f9d95b09f63c6c0
vidhi-parikh <parikhvidhi10@gmail.com>
2024-05-23 23:53:44 +0530

    feat: create UserContext for state management

---
+++ frontend/src/context/UserContext.js
@@ -0,0 +1,34 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    setUser(userInfo);
+
+    if (!userInfo) {
+      navigate("/");
+    }
+  }, [navigate]);
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        setUser,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const UserState = () => {
+  return useContext(UserContext);
+};
+
+export default UserProvider;
